f9e6b83d7d525285b5ae42e7bd3e65d81cc790de
Godly <68631116+Godly-GM@users.noreply.github.com>
2022-04-30 10:50:10 +0530

    Add files via upload

---
+++ 10.sh
@@ -0,0 +1,11 @@
+echo "Program 10"
+echo "understaing difference between echo statements using "",'', without quotes"
+echo " "
+
+a="Ubuntu"
+
+echo "Word is " $a
+echo " "
+echo "Without quotes: " $a
+echo "With Single Quotes: " '$a'
+echo "With Double Quotes: " "$a"
